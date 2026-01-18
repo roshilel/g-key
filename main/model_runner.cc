ba80db680869dca22f49d3ffdcaac43899e2a1ad
roshilel <aronmaung@gmail.com>
2026-01-17 16:28:39 -0800

    Fixed model ops and allocation

--- main/model_runner.cc
+++ main/model_runner.cc
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
+#include "esp_heap_caps.h"
 #include "model_runner.h"
 #include "tensorflow/lite/c/c_api_types.h"
 #include "tensorflow/lite/c/common.h"
-#include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
@@ -16,39 +16,56 @@ static TfLiteTensor *input_tensor_ptr =
 
 using NextKeyOpsResolver = tflite::MicroMutableOpResolver<20>;
 
-// operations for LSTM
+// operations for Conv1D
 static TfLiteStatus RegisterOps(NextKeyOpsResolver &op_resolver) {
   TF_LITE_ENSURE_STATUS(op_resolver.AddGather());
-  TF_LITE_ENSURE_STATUS(op_resolver.AddUnidirectionalSequenceLSTM());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddConv2D());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddReshape());
   TF_LITE_ENSURE_STATUS(op_resolver.AddFullyConnected());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddRelu());
   TF_LITE_ENSURE_STATUS(op_resolver.AddSoftmax());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddLess());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddAdd());
+  TF_LITE_ENSURE_STATUS(op_resolver.AddDequantize());
   return kTfLiteOk;
 }
+
+constexpr int kTensorArenaSize = 60 * 1024;
+uint8_t *tensor_arena = nullptr;
 
 void model_setup() {
 
   // reading model byte array
-  extern const unsigned char nextkey_tflite[];
-  extern const unsigned int nextkey_tflite_len;
-  const tflite::Model *model = tflite::GetModel(nextkey_tflite);
+  extern const unsigned char conv1d_nonhybrid_tflite[];
+  const tflite::Model *model = tflite::GetModel(conv1d_nonhybrid_tflite);
 
   // adding operations required for model
-  NextKeyOpsResolver op_resolver;
-
+  static NextKeyOpsResolver op_resolver;
   TfLiteStatus status = RegisterOps(op_resolver);
   if (status != kTfLiteOk) {
     MicroPrintf("Op registration failed");
     return;
   }
 
-  // defining mem needed
-  // constexpr: no need to give this var mem address
-  constexpr uint16_t tensor_arena_size = 4 * 1024; // 4MB
-  uint8_t tensor_arena[tensor_arena_size];
+  // allocate in to PSRAM 
+  MicroPrintf("Attempting to allocate %d bytes in PSRAM...", kTensorArenaSize);
+  tensor_arena = (uint8_t *)heap_caps_malloc(
+      kTensorArenaSize, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+
+  // fallback to heap
+  if (tensor_arena == nullptr) {
+    MicroPrintf("PSRAM allocation failed. Falling back to heap.");
+    tensor_arena = (uint8_t *)malloc(kTensorArenaSize);
+  }
+
+  if (tensor_arena == nullptr) {
+    MicroPrintf("Failed to allocate tensor arena");
+    return;
+  }
 
   // initializing interpreter
   static tflite::MicroInterpreter interpreter(model, op_resolver, tensor_arena,
-                                              tensor_arena_size);
+                                              kTensorArenaSize);
   // allocating memory
   interpreter.AllocateTensors();
 
@@ -82,6 +99,19 @@ void model_setup() {
   }
 
   input_tensor_ptr = temp_input_tensor_ptr;
+
+  for (int i = 0; i < 40; i++) {
+    input_tensor_ptr->data.i32[i] = 0; 
+  }
+
+  TfLiteStatus invoke_status = interpreter_ptr->Invoke();
+
+  if (invoke_status != kTfLiteOk) {
+      MicroPrintf("Invoke failed");
+  } else {
+      MicroPrintf("Invoked successfully");
+  }
+
 }
 
 int logTest(int x) {
