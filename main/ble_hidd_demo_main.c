2f7aecf2c70d797533262b4d88a63f171c9db0e2
roshilel <aronmaung@gmail.com>
2026-02-25 20:54:47 -0800

    Integrated model inference pipeline with BLE, allocated larger custom partition

--- main/ble_hidd_demo_main.c
+++ main/ble_hidd_demo_main.c
@@ -192,16 +192,37 @@ static void gap_event_handler(esp_gap_ble_cb_event_t event,
 
 void hid_demo_task(void *pvParameters) {
   vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+  char input_buffer[41] = {0};
+  input_buffer[40] = '\0';
+  size_t input_idx = 0;
+
   while (1) {
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    vTaskDelay(pdMS_TO_TICKS(10));
     if (sec_conn) { // checking for secure connection
-      char *thisTaskName = pcTaskGetName(NULL);
-      ESP_LOGI(thisTaskName, "ESP-32 KEYBOARD connected");
-      uint8_t g_key[1] = {HID_KEY_G};
 
-      esp_hidd_send_keyboard_value(hid_conn_id, 0, g_key, 1);
+      uint8_t data;
 
-      esp_hidd_send_keyboard_value(hid_conn_id, 0, NULL, 0);
+      if (uart_read_bytes(UART_NUM_0, &data, 1, 0) > 0) {
+        char c = (char)data;
+        if (input_idx < 40) {
+          input_buffer[input_idx] = c;
+          input_idx++;
+        } else {
+          memmove(input_buffer, input_buffer + 1, 39);
+          input_buffer[39] = c;
+        }
+        int32_t pred_char = run_inference(input_buffer, input_idx);
+        ESP_LOGI(HID_DEMO_TAG, "predicted character: %c", (char)pred_char);
+      }
+
+      // char *thisTaskName = pcTaskGetName(NULL);
+      // ESP_LOGI(thisTaskName, "ESP-32 KEYBOARD connected");
+      // uint8_t g_key[1] = {HID_KEY_G};
+      //
+      // esp_hidd_send_keyboard_value(hid_conn_id, 0, g_key, 1);
+      //
+      // esp_hidd_send_keyboard_value(hid_conn_id, 0, NULL, 0);
       // send_volum_up = true;
       // // uint8_t key_vaule = {HID_KEY_A};
       // // esp_hidd_send_keyboard_value(hid_conn_id, 0, &key_vaule, 1);
@@ -239,104 +260,81 @@ void app_main(void) {
 
   model_setup();
 
-  char input_buffer[41] = {0};
-  input_buffer[40] = '\0';
-  size_t input_idx = 0;
+  esp_err_t ret;
 
-  while (1) {
-    uint8_t data;
+  // Initialize NVS.
+  // non-volatile storage (nvs)
+  ret = nvs_flash_init(); // stores bonded devices
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 
-    if (uart_read_bytes(UART_NUM_0, &data, 1, 0) > 0) {
-      char c = (char)data;
-      if (input_idx < 40) {
-        input_buffer[input_idx] = c;
-        input_idx++;
-      } else {
-        memmove(input_buffer, input_buffer + 1, 39);
-        input_buffer[39] = c;
-      }
-      int32_t pred_char = run_inference(input_buffer, input_idx);
-      ESP_LOGI(HID_DEMO_TAG, "predicted character: %c", (char)pred_char);
-    }
+  ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
 
-    vTaskDelay(pdMS_TO_TICKS(10));
+  esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+
+  // initializing hardware
+  ret = esp_bt_controller_init(&bt_cfg);
+  if (ret) {
+    ESP_LOGE(HID_DEMO_TAG, "%s initialize controller failed", __func__);
+    return;
   }
 
-  // esp_err_t ret;
-  //
-  // // Initialize NVS.
-  // // non-volatile storage (nvs)
-  // ret = nvs_flash_init(); // stores bonded devices
-  // if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-  //     ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-  //   ESP_ERROR_CHECK(nvs_flash_erase());
-  //   ret = nvs_flash_init();
-  // }
-  // ESP_ERROR_CHECK(ret);
-  //
-  // ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-  //
-  // esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-  //
-  // // initializing hardware
-  // ret = esp_bt_controller_init(&bt_cfg);
-  // if (ret) {
-  //   ESP_LOGE(HID_DEMO_TAG, "%s initialize controller failed", __func__);
-  //   return;
-  // }
-  //
-  // ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-  // if (ret) {
-  //   ESP_LOGE(HID_DEMO_TAG, "%s enable controller failed", __func__);
-  //   return;
-  // }
-  //
-  // esp_bluedroid_config_t cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
-  // ret = esp_bluedroid_init_with_cfg(&cfg);
-  // if (ret) {
-  //   ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
-  //   return;
-  // }
-  //
-  // ret = esp_bluedroid_enable();
-  // if (ret) {
-  //   ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
-  //   return;
-  // }
-  //
-  // if ((ret = esp_hidd_profile_init()) != ESP_OK) {
-  //   ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
-  // }
-  //
-  // /// register the callback function to the gap module
-  // esp_ble_gap_register_callback(gap_event_handler);
-  // esp_hidd_register_callbacks(hidd_event_callback);
-  //
-  // /* set the security iocap & auth_req & key size & init key response key
-  //  * parameters to the stack*/
-  // esp_ble_auth_req_t auth_req =
-  //     ESP_LE_AUTH_BOND; // bonding with peer device after authentication
-  // esp_ble_io_cap_t iocap =
-  //     ESP_IO_CAP_NONE;   // set the IO capability to No output No input
-  // uint8_t key_size = 16; // the key size should be 7~16 bytes
-  // uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
-  // uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
-  // esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req,
-  //                                sizeof(uint8_t));
-  // esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap,
-  //                                sizeof(uint8_t));
-  // esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size,
-  //                                sizeof(uint8_t));
-  // /* If your BLE device act as a Slave, the init_key means you hope which
-  // types of key of the master should distribute to you, and the response key
-  // means which key you can distribute to the Master; If your BLE device act as
-  // a master, the response key means you hope which types of key of the slave
-  // should distribute to you, and the init key means which key you can
-  // distribute to the slave. */
-  // esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key,
-  //                                sizeof(uint8_t));
-  // esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key,
-  //                                sizeof(uint8_t));
-  //
-  // xTaskCreate(&hid_demo_task, "hid_task", 2048, NULL, 5, NULL);
+  ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+  if (ret) {
+    ESP_LOGE(HID_DEMO_TAG, "%s enable controller failed", __func__);
+    return;
+  }
+
+  esp_bluedroid_config_t cfg = BT_BLUEDROID_INIT_CONFIG_DEFAULT();
+  ret = esp_bluedroid_init_with_cfg(&cfg);
+  if (ret) {
+    ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
+    return;
+  }
+
+  ret = esp_bluedroid_enable();
+  if (ret) {
+    ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
+    return;
+  }
+
+  if ((ret = esp_hidd_profile_init()) != ESP_OK) {
+    ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed", __func__);
+  }
+
+  /// register the callback function to the gap module
+  esp_ble_gap_register_callback(gap_event_handler);
+  esp_hidd_register_callbacks(hidd_event_callback);
+
+  /* set the security iocap & auth_req & key size & init key response key
+   * parameters to the stack*/
+  esp_ble_auth_req_t auth_req =
+      ESP_LE_AUTH_BOND; // bonding with peer device after authentication
+  esp_ble_io_cap_t iocap =
+      ESP_IO_CAP_NONE;   // set the IO capability to No output No input
+  uint8_t key_size = 16; // the key size should be 7~16 bytes
+  uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+  uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+  esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req,
+                                 sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap,
+                                 sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size,
+                                 sizeof(uint8_t));
+  /* If your BLE device act as a Slave, the init_key means you hope which
+  types of key of the master should distribute to you, and the response key
+  means which key you can distribute to the Master; If your BLE device act as
+  a master, the response key means you hope which types of key of the slave
+  should distribute to you, and the init key means which key you can
+  distribute to the slave. */
+  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key,
+                                 sizeof(uint8_t));
+  esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key,
+                                 sizeof(uint8_t));
+
+  xTaskCreate(&hid_demo_task, "hid_task", 16384, NULL, 5, NULL);
 }
